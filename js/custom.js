92b2714c65f3085422c4f898642cc408d8f25cf2
akramulfata10 <superlay582@gmail.com>
2021-12-11 18:26:17 +0700

    menambahkan slick slider

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(".slick_slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  speed: 1500,
+  autoplaySpeed: 2000,
+  prevArrow: '<i class="bi bi-caret-left left_arrow"></i>',
+  nextArrow: '<i class="bi bi-caret-right right_arrow"></i>',
+});
