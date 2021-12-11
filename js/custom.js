c11937476ba8a31d66a05e0bbd23f5e1f3b38f39
akramulfata10 <superlay582@gmail.com>
2021-12-11 22:38:32 +0700

    menambah kan halaman baru

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,33 @@ $(".slick_slider").slick({
   slidesToScroll: 2,
   autoplay: true,
   speed: 1000,
-  autoplaySpeed: 1000,
+  autoplaySpeed: 2000,
   prevArrow: '<i class="bi bi-caret-left left_arrow"></i>',
   nextArrow: '<i class="bi bi-caret-right right_arrow"></i>',
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
 });
