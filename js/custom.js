ead5c42a64951b9292b9c37323089d0be22b9b4d
akramulfata10 <superlay582@gmail.com>
2021-12-11 19:03:10 +0700

    nambah lah

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,8 @@ $(".slick_slider").slick({
   slidesToShow: 2,
   slidesToScroll: 2,
   autoplay: true,
-  speed: 1500,
-  autoplaySpeed: 2000,
+  speed: 1000,
+  autoplaySpeed: 1000,
   prevArrow: '<i class="bi bi-caret-left left_arrow"></i>',
   nextArrow: '<i class="bi bi-caret-right right_arrow"></i>',
 });
